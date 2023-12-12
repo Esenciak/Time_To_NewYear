073f71e361e77541d77b4279cdc57fab3223f279
Karol Jabłoński <Karol.jablonski00@gmail.com>
2023-12-12 14:14:04 +0100

    commit

--- Liczenie do nowego roku/Liczenie_do_nowego_roku.py
+++ Liczenie do nowego roku/Liczenie_do_nowego_roku.py
@@ -1,3 +1,4 @@
+
 from datetime import datetime
 import tkinter as tk
 
@@ -19,20 +20,20 @@ def liczenie():
     canvas.after(1000, liczenie)
 
 def rysuj_choinke(wysokosc_choinki):
-    for i in range(1, min(wysokosc_choinki, 10) + 1):  
-        x = (wysokosc_choinki - i) * 20
-        y = i * 40
-        szerokosc_gwiazdki = 40
+    if dzis.day == swieta.day and dzis.month == swieta.month:
+        for i in range(1, min(wysokosc_choinki, 10) + 1):  
+            x = (wysokosc_choinki - i) * 20
+            y = i * 40
+            szerokosc_gwiazdki = 40
 
-        odstep_miedzy_gwiazdkami = 20
-        x_gwiazdki = x
-        for _ in range(2 * i - 1):
-            canvas.create_text(x_gwiazdki, y, text="*", font=("Courier", 14), anchor="w")
-            x_gwiazdki += odstep_miedzy_gwiazdkami
+            odstep_miedzy_gwiazdkami = 20
+            x_gwiazdki = x
+            for _ in range(2 * i - 1):
+                canvas.create_text(x_gwiazdki, y, text="*", font=("Courier", 14), anchor="w")
+                x_gwiazdki += odstep_miedzy_gwiazdkami
 
-    
-    canvas.create_rectangle((min(wysokosc_choinki, 10) - 1) * 20 + 15, min(wysokosc_choinki, 10) * 40,
-                            (min(wysokosc_choinki, 10) - 1) * 20 + 25, min(wysokosc_choinki, 10) * 40 + 40, fill="brown")
+        canvas.create_rectangle((min(wysokosc_choinki, 10) - 1) * 20 + 15, min(wysokosc_choinki, 10) * 40,
+                                (min(wysokosc_choinki, 10) - 1) * 20 + 25, min(wysokosc_choinki, 10) * 40 + 40, fill="brown")
 
 def rysuj_choinke_button():
     wysokosc_choinki = int(entry_wysokosc_choinki.get())
@@ -44,9 +45,6 @@ okno.title("Ile do sylwestra by Karol Jablonski")
 etykieta = tk.Label(okno, font=("Helvetica", 14), pady=20)
 etykieta.pack()
 
-label_wysokosc_choinki = tk.Label(okno, text="Wysokosc choinki:")
-label_wysokosc_choinki.pack()
-
 entry_wysokosc_choinki = tk.Entry(okno)
 entry_wysokosc_choinki.pack()
 
@@ -55,8 +53,11 @@ entry_wysokosc_choinki.insert(0, "5")
 canvas = tk.Canvas(okno, width=400, height=400)
 canvas.pack()
 
-button_rysuj_choinke = tk.Button(okno, text="Rysuj choinke", command=rysuj_choinke_button)
-button_rysuj_choinke.pack()
+dzis = datetime.now()
+swieta = datetime(dzis.year, 12, 24)
+if dzis.day == swieta.day and dzis.month == swieta.month:
+    button_rysuj_choinke = tk.Button(okno, text="Rysuj choinke ", command=rysuj_choinke_button)
+    button_rysuj_choinke.pack()
 
 liczenie()
 
